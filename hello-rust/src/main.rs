eefa776b4d52cce5376ada0ec99269068785075e
Sho Kohara <shokohara@users.noreply.github.com>
2022-01-26 04:03:14 +0900

    .

--- hello-rust/src/main.rs
+++ hello-rust/src/main.rs
@@ -20,7 +20,7 @@ fn main() -> Result<()> {
         MessageBoxA(None, "Text", "Caption", MB_OK);
     }
 
-    let hWndParent = GetDesktopWindow::new();
+    let hWndParent = GetDesktopWindow();
 
     Ok(())
 }
