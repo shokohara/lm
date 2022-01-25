6eac79b4676edafa7cf9578bd2acea227041a2e8
Sho Kohara <shokohara@users.noreply.github.com>
2022-01-26 04:01:32 +0900

    .

--- hello-rust/src/main.rs
+++ hello-rust/src/main.rs
@@ -1,44 +1,24 @@
-//use ferris_says::say;
-//use std::io::{stdout, BufWriter};
-//
-//fn main() {
-//    let stdout = stdout();
-//    let message = String::from("Hello fellow Rustaceans!");
-//    let width = message.chars().count();
-//
-//    let mut writer = BufWriter::new(stdout.lock());
-//    say(message.as_bytes(), width, &mut writer).unwrap();
-//}
-
-// use bindings::{
-//     Windows::Foundation::Uri,
-//     Windows::Web::Syndication::SyndicationClient,
-//     Windows::Win32::UI::WindowsAndMessaging::GetDesktopWindow,
-// };
-//
-// fn main() -> windows::Result<()> {
-//     let uri = Uri::CreateUri("https://blogs.windows.com/feed")?;
-//     let client = SyndicationClient::new()?;
-//     let feed = client.RetrieveFeedAsync(uri)?.get()?;
-//
-//     for item in feed.Items()? {
-//         println!("{}", item.Title()?.Text()?);
-//     }
-//     let hWndParent = GetDesktopWindow::new();
-//     Ok(())
-// }
-
 use windows::{
+    core::*, Data::Xml::Dom::*, Win32::Foundation::*, Win32::System::Threading::*,
     Win32::UI::WindowsAndMessaging::*,
 };
-fn main() -> windows::Result<()> {
-    // let uri = Uri::CreateUri("https://blogs.windows.com/feed")?;
-    // let client = SyndicationClient::new()?;
-    // let feed = client.RetrieveFeedAsync(uri)?.get()?;
-    //
-    // for item in feed.Items()? {
-    //     println!("{}", item.Title()?.Text()?);
-    // }
-    // let hWndParent = GetDesktopWindow::new();
+
+fn main() -> Result<()> {
+    let doc = XmlDocument::new()?;
+    doc.LoadXml("<html>hello world</html>")?;
+
+    let root = doc.DocumentElement()?;
+    assert!(root.NodeName()? == "html");
+    assert!(root.InnerText()? == "hello world");
+
+    unsafe {
+        let event = CreateEventW(std::ptr::null_mut(), true, false, None);
+        SetEvent(event).ok()?;
+        WaitForSingleObject(event, 0);
+        CloseHandle(event).ok()?;
+
+        MessageBoxA(None, "Text", "Caption", MB_OK);
+    }
+
     Ok(())
 }
