22cedda4c15646204827002cf46c1f787dfdf617
Sho Kohara <shokohara@users.noreply.github.com>
2022-01-26 04:00:25 +0900

    .

--- hello-rust/src/main.rs
+++ hello-rust/src/main.rs
@@ -10,20 +10,35 @@
 //    say(message.as_bytes(), width, &mut writer).unwrap();
 //}
 
-use bindings::{
-    Windows::Foundation::Uri,
-    Windows::Web::Syndication::SyndicationClient,
-    Windows::Win32::UI::WindowsAndMessaging::GetDesktopWindow,
+// use bindings::{
+//     Windows::Foundation::Uri,
+//     Windows::Web::Syndication::SyndicationClient,
+//     Windows::Win32::UI::WindowsAndMessaging::GetDesktopWindow,
+// };
+//
+// fn main() -> windows::Result<()> {
+//     let uri = Uri::CreateUri("https://blogs.windows.com/feed")?;
+//     let client = SyndicationClient::new()?;
+//     let feed = client.RetrieveFeedAsync(uri)?.get()?;
+//
+//     for item in feed.Items()? {
+//         println!("{}", item.Title()?.Text()?);
+//     }
+//     let hWndParent = GetDesktopWindow::new();
+//     Ok(())
+// }
+
+use windows::{
+    Win32::UI::WindowsAndMessaging::*,
 };
-
 fn main() -> windows::Result<()> {
-    let uri = Uri::CreateUri("https://blogs.windows.com/feed")?;
-    let client = SyndicationClient::new()?;
-    let feed = client.RetrieveFeedAsync(uri)?.get()?;
-
-    for item in feed.Items()? {
-        println!("{}", item.Title()?.Text()?);
-    }
-    let hWndParent = GetDesktopWindow::new();
+    // let uri = Uri::CreateUri("https://blogs.windows.com/feed")?;
+    // let client = SyndicationClient::new()?;
+    // let feed = client.RetrieveFeedAsync(uri)?.get()?;
+    //
+    // for item in feed.Items()? {
+    //     println!("{}", item.Title()?.Text()?);
+    // }
+    // let hWndParent = GetDesktopWindow::new();
     Ok(())
 }
